07bddff197f6adeb2627ba42c6d71be6a386dfd3
unknown <bandanathapa80@gmail.com>
2022-09-07 11:14:49 +0545

    first commit

---
+++ intro.py
@@ -0,0 +1,17 @@
+# This is a sinlge line comment
+"""
+This is a multiline comment
+we can write many lines here as much as you want init
+no matter 
+what
+This all above lines are considered ars
+comment in python
+"""
+
+print("Hello! Batch 33B")
+print(2+2)
+
+name = input(Please provide your name:)
+age = input(Please provide your age: )
+print(f Hi!your name is {name}, and you are {age}, years old) #String Formatting
+
